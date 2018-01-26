068727d8bb6a136acfbe1adf51c93686a8b906ff
Sean <seansailer28@gmail.com>
2018-01-26 11:41:08 -0600

    Testing git push

---
+++ src/chess/Queen.java
@@ -0,0 +1,17 @@
+package chess;
+
+public class Queen implements Piece {
+
+	@Override
+	public String getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLegalMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
